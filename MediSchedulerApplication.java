b04bf9ad6825707501255e42107e9f83b4337bb7
vamsi-bits <2021wb86466@wilp.bits-pilani.ac.in>
2025-07-09 19:01:25 +0530

    Add files via upload

---
+++ MediSchedulerApplication.java
@@ -0,0 +1,11 @@
+package com.example.medischedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediSchedulerApplication.class, args);
+    }
+}
